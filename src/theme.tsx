f114c4e89059e5de687422195164454077e39315
Mark Chandler <mark@chanchan.io>
2021-01-16 11:19:30 -0500

    refactor: use theme.styles.global to adjust font size

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,20 +1,12 @@
 import {extendTheme} from '@chakra-ui/react'
 
 export const theme = extendTheme({
-  fontSizes: {
-    xs: '0.875rem',
-    sm: '1rem',
-    md: '1.125rem',
-    lg: '1.25rem',
-    xl: '1.5rem',
-    '2xl': '1.875rem',
-    '3xl': '2.25rem',
-    '4xl': '3rem',
-    '5xl': '3.75rem',
-    '6xl': '4.5rem',
-    '7xl': '6rem',
-    '8xl': '8rem',
-    '9xl': '9rem',
+  styles: {
+    global: {
+      html: {
+        fontSize: '1.125rem',
+      },
+    },
   },
   components: {
     Badge: {
