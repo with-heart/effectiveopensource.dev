1f67c7662ed43d40311b59996ce3a43c5916533b
Mark Chandler <github@chanchan.io>
2021-06-04 08:05:59 -0400

    chore: create packages for rehype plugins

--- site/next-env.d.ts
+++ site/next-env.d.ts
@@ -2,3 +2,5 @@
 /// <reference types="next/types/global" />
 
 declare module '@silvenon/remark-smartypants'
+declare module '@effectiveopensource/rehype-highlight-code'
+declare module '@effectiveopensource/rehype-meta-attribute'
--- site/src/lib/markdown.ts
+++ site/src/lib/markdown.ts
@@ -1,9 +1,9 @@
+import rehypeHighlightCode from '@effectiveopensource/rehype-highlight-code'
+import rehypeMetaAttribute from '@effectiveopensource/rehype-meta-attribute'
 import {findRootSync} from '@manypkg/find-root'
 import smartypants from '@silvenon/remark-smartypants'
 import {bundleMDX} from 'mdx-bundler'
 import path from 'path'
-import rehypeHighlightCode from '../../rehype/rehype-highlight-code'
-import rehypeMetaAttribute from '../../rehype/rehype-meta-attribute'
 
 process.env.ESBUILD_BINARY_PATH = path.join(
   // point to root `node_modules` since this project uses yarn workspaces
