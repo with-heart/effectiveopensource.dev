8594d07593530fed1880b5536be773cef426b5fe
Mark Chandler <github@chanchan.io>
2021-06-22 18:02:36 -0400

    chore(site): restructure module declarations

---
+++ site/modules.d.ts
@@ -0,0 +1,3 @@
+declare module '@silvenon/remark-smartypants'
+declare module '@effectiveopensource/rehype-highlight-code'
+declare module '@effectiveopensource/rehype-meta-attribute'
--- site/next-env.d.ts
+++ site/next-env.d.ts
@@ -1,6 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module '@silvenon/remark-smartypants'
-declare module '@effectiveopensource/rehype-highlight-code'
-declare module '@effectiveopensource/rehype-meta-attribute'
+/// <reference types="next/image-types/global" />
