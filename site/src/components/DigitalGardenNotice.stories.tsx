203c916c958ead424ba0c5a1f841b10a2bd85585
with-heart <with.heart+git@pm.me>
2021-10-07 22:04:09 -0400

    chore(storybook): add stories for main components

---
+++ site/src/components/DigitalGardenNotice.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {DigitalGardenNotice} from './DigitalGardenNotice'
+
+export default {
+  title: 'Components/DigitalGardenNotice',
+  component: DigitalGardenNotice,
+}
+
+export const Default = () => <DigitalGardenNotice />
